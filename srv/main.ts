befd34e3aa5d6c5b7da507c594342689cc49caec
Jean Cardoso de Souza <blahjeancs1@hotmail.com>
2026-01-09 18:23:25 -0300

    feat: add after read customers logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,21 +1,23 @@
-import { Customer, Customers } from '@models/sales';
 
-const customer: Customer = {
-    email: 'jean@gmailcom',
-    firstName: 'Jean',
-    lastName: 'Souza',
-    id: '1234'
+
+
+
+
+import { Service } from '@sap/cds';
+import { Customers } from '@models/sales';
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers[]) => {
+        //console.log('ARRAY COMPLETO:', results);
+        //console.log('ÚLTIMO REGISTRO:', results.at(-1));
+        
+        results.forEach(customer => {
+            if (!customer.email?.includes('@')) {
+                customer.email = `${customer.email}@gmail.com`;
+            }
+        });
+    });
 };
 
-const customers: Customers [customer]
 
-const funcao = (variavel: string) => console.log(variavel);
-funcao('123');
 
-export  (service) => {
-    service.on('CREATE','SalesOrderHeaders', () =>); //sobresescrever o comportamento de criação
-    service.before(['CREATE', 'UPDATE'], 'SalesOrderHeaders', () =>); //sobresescrever o comportamento de criação
-    service.after('DELETE',['SalesOrderHeaders', 'Customers'] ,); //sobresescrever o comportamento de criação
-    //codigo vai aqui
-    //sobresescrever o comportamento de criação
-}
