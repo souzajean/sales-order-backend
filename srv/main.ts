0ca39af5f3c8ea8514e61ecc2566414a95135fbd
Jean Cardoso de Souza <blahjeancs1@hotmail.com>
2026-01-09 17:26:52 -0300

    feat: add main.ts

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,21 @@
-const funcao = (variavel: string) => console.log(variavel);
+import { Customer, Customers } from '@models/sales';
 
+const customer: Customer = {
+    email: 'jean@gmailcom',
+    firstName: 'Jean',
+    lastName: 'Souza',
+    id: '1234'
+};
+
+const customers: Customers [customer]
+
+const funcao = (variavel: string) => console.log(variavel);
 funcao('123');
+
+export  (service) => {
+    service.on('CREATE','SalesOrderHeaders', () =>); //sobresescrever o comportamento de criação
+    service.before(['CREATE', 'UPDATE'], 'SalesOrderHeaders', () =>); //sobresescrever o comportamento de criação
+    service.after('DELETE',['SalesOrderHeaders', 'Customers'] ,); //sobresescrever o comportamento de criação
+    //codigo vai aqui
+    //sobresescrever o comportamento de criação
+}
