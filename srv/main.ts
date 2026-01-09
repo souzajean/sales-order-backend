50ba6c596ea4aa7a7e4fdc0cc1b74da41218c99d
Jean Cardoso de Souza <blahjeancs1@hotmail.com>
2026-01-09 20:21:06 -0300

    feat: add main.ts Validacao Custumers e Items

--- srv/main.ts
+++ srv/main.ts
@@ -1,23 +1,46 @@
 
-
-
-
-
-import { Service } from '@sap/cds';
+import cds, { Request, Service } from '@sap/cds';
 import { Customers } from '@models/sales';
 
 export default (service: Service) => {
+
     service.after('READ', 'Customers', (results: Customers[]) => {
         //console.log('ARRAY COMPLETO:', results);
         //console.log('ÚLTIMO REGISTRO:', results.at(-1));
-        
         results.forEach(customer => {
             if (!customer.email?.includes('@')) {
                 customer.email = `${customer.email}@gmail.com`;
             }
         });
     });
+
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+
+        // 1️⃣ valida customer_id
+        if (!params.customer_id) {
+            return request.reject(400, 'Customer inválido');
+        }
+
+        // 2️⃣ valida itens
+        if (!params.items || params.items.length === 0) {
+            return request.reject(400, 'Itens inválidos');
+        }
+
+        // 3️⃣ busca customer no banco
+        const customer = await cds.run(
+            cds.ql.SELECT.one
+                .from('sales.Customers')
+                .where({ id: params.customer_id })
+        );
+
+        // 4️⃣ valida existência do customer
+        if (!customer) {
+            return request.reject(404, 'Customer não encontrado');
+        }
+
+        // opcional: log
+        console.log('Customer encontrado:', customer);
+    });
+
 };
-
-
-
